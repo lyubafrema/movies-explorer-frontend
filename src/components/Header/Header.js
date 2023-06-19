afbac977e341c10a5783c832fd3db89405aa5867
Lyuba Efremova <lyuba.smile.97@yandex.ru>
2023-06-20 00:37:59 +0300

    feat: preloader

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -9,10 +9,12 @@ export default function Header() {
   const isLanding = location.pathname === '/';
   return (
     <header className={`header ${isLanding ? "header_landing" : ""}`}>
-      <Link className="header__logo-link" to="/" >
-        <img className="header__logo" src={headerLogo} alt="Логотип: Синий квадрат с улыбкой." />
-      </Link>
-      {isLanding ? <Navigation /> : <NavigationMain />}
+      <div className="header__container">
+        <Link className="header__logo-link" to="/" >
+          <img className="header__logo" src={headerLogo} alt="Логотип: Синий квадрат с улыбкой." />
+        </Link>
+        {isLanding ? <Navigation /> : <NavigationMain />}
+      </div>
     </header>
   )
 }
